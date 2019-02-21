96414823bd81829448d7ef505df5ac1002783be5
Ramon Duraes <ramongduraes@gmail.com>
2019-02-21 16:42:14 -0300

    Fixing buildproto

--- buildproto.sh
+++ buildproto.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3.6 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. service/service_spec/style_transfer.proto
+python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. service/service_spec/style_transfer.proto
